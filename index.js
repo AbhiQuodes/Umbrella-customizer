659e286562682c2ae749f6523a7eafbc0418e7d8
AbhiQuodes <bhartiabhinav618@gmail.com>
2024-09-15 07:38:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let colorOptions = document.querySelectorAll('.color-Options');
+let customise_umbrella =document.querySelector('#customised-umbrella');
+const maxImgSize =5 * 1024 * 1024;
+
+function rotateUmbrella()
+{
+    customise_umbrella.style="animation='rotation 1.5s 1 ease-in-out'";
+
+}
+
+
+Array.from(colorOptions).forEach(colorOption=>{
+    colorOption.addEventListener('click',()=>{
+
+    })
+})

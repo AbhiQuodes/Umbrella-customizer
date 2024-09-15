d680c2bb2af704083c0b418842006fd7a56a8058
AbhiQuodes <bhartiabhinav618@gmail.com>
2024-09-15 13:01:37 +0530

    added image download button

--- index.js
+++ index.js
@@ -1,78 +1,85 @@
-let colorOptions = document.querySelectorAll('.color-Options');
-let colorSelects = document.querySelector('.color-Select');
-let getLogo = document.querySelector('#get-logo');
-let fileName,imgSrc;
-let media_box= document.querySelector('.media-box');
-let display_logo =document.querySelector('.display-logo');
-let customise_umbrella =document.querySelector('#customised-umbrella');
-const maxImgSize =5 * 1024 * 1024;
+let colorOptions = document.querySelectorAll(".color-Options");
+let colorSelects = document.querySelector(".color-Select");
+let getLogo = document.querySelector("#get-logo");
+let downloadBtn = document.querySelector('#download');
+let fileName, imgSrc;
+let media_box = document.querySelector(".media-box");
+let display_logo = document.querySelector(".display-logo");
+let customise_umbrella = document.querySelector("#customised-umbrella");
+const maxImgSize = 5 * 1024 * 1024;
 
-function rotateUmbrella()
-{
-    media_box.classList.add('rotate');
-    setTimeout(()=>{
-        media_box.classList.remove('rotate');
 
-    },1500)
-
+function rotateUmbrella() {
+  media_box.classList.add("rotate");
+  setTimeout(() => {
+    media_box.classList.remove("rotate");
+  }, 1500);
 }
 
-function changeUmbrellaColor(imgSrc){
-    setTimeout(()=>{ 
-        customise_umbrella.src=imgSrc;
-    }, 700);
+function changeUmbrellaColor(imgSrc) {
+  setTimeout(() => {
+    customise_umbrella.src = imgSrc;
+  }, 700);
 }
 
-
-Array.from(colorOptions).forEach(colorOption=>{
-    colorOption.addEventListener('pointerdown',(e)=>{
-        if(e.target.id=='color-Pink')
-        {
-            changeUmbrellaColor('./assets/pink_umbrella.png');
-            colorSelects.style.borderColor='Pink'
-            
-        }
-        else if(e.target.id=='color-Blue')
-            {
-            changeUmbrellaColor('./assets/blue_umbrella.png');
-            colorSelects.style.borderColor='Blue'
-        }
-        else
-        {
-            changeUmbrellaColor('./assets/yellow_umbrella.png');
-            colorSelects.style.borderColor='Yellow'
-        }
-
-        rotateUmbrella();
-
-    })
-})
-
-
-getLogo.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    if (file) {
-        if (file.size > maxImgSize) {
-            alert("File size exceeds 5MB. Please upload a smaller file.");
-            getLogo.value = ""; 
-        } else {
-            reader.onload = function(e) {
-                const logoImg = document.createElement('img');
-                logoImg.src = e.target.result;
-                logoImg.style.position = 'relative';
-                logoImg.style.transform = 'translate(-50%, -50%)';
-                logoImg.style.width = '50px'; 
-                logoImg.style.height = '30px';
-                logoImg.style.top = '-40px'; 
-                logoImg.style.left='25px'
-                
-                display_logo.innerHTML = '';
-                
-                display_logo.appendChild(logoImg);
-            };
-            reader.readAsDataURL(file);
-        }
+Array.from(colorOptions).forEach((colorOption) => {
+  colorOption.addEventListener("pointerdown", (e) => {
+    if (e.target.id == "color-Pink") {
+      changeUmbrellaColor("./assets/pink_umbrella.png");
+      colorSelects.style.borderColor = "Pink";
+    } else if (e.target.id == "color-Blue") {
+      changeUmbrellaColor("./assets/blue_umbrella.png");
+      colorSelects.style.borderColor = "Blue";
+    } else {
+      changeUmbrellaColor("./assets/yellow_umbrella.png");
+      colorSelects.style.borderColor = "Yellow";
     }
+
+    rotateUmbrella();
+  });
+});
+
+getLogo.addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  if (file) {
+    if (file.size > maxImgSize) {
+      alert("File size exceeds 5MB. Please upload a smaller file.");
+      getLogo.value = "";
+    } else {
+      reader.onload = function (e) {
+        const logoImg = document.createElement("img");
+        logoImg.src = e.target.result;
+        logoImg.style.position = "relative";
+        logoImg.style.transform = "translate(-50%, -50%)";
+        logoImg.style.width = "50px";
+        logoImg.style.height = "30px";
+        logoImg.style.top = "-40px";
+        logoImg.style.left = "25px";
+
+        display_logo.innerHTML = "";
+
+        display_logo.appendChild(logoImg);
+        downloadBtn.style.display='flex';
+      };
+      reader.readAsDataURL(file);
+
+    }
+  }
+});
+
+document.getElementById('download').addEventListener('click', function() {
+    html2canvas(document.getElementById('media-id')).then(function(canvas) {
+        // Create an image element
+        var img = canvas.toDataURL('image/png');
+        
+        // Create a download link
+        var link = document.createElement('a');
+        link.href = img;
+        link.download = 'Umbrella.png';
+        
+        // Trigger the download
+        link.click();
+    });
 });

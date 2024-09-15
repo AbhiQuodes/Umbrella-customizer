73cec2b7eb97b0ace703f18d0c0ffb98c54772a5
AbhiQuodes <bhartiabhinav618@gmail.com>
2024-09-15 10:29:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,75 @@
 let colorOptions = document.querySelectorAll('.color-Options');
+let colorSelects = document.querySelector('.color-Select');
+let getLogo = document.querySelector('#get-logo');
+let fileName,imgSrc;
+let media_box= document.querySelector('.media-box');
+let display_logo =document.querySelector('.display-logo');
 let customise_umbrella =document.querySelector('#customised-umbrella');
 const maxImgSize =5 * 1024 * 1024;
 
 function rotateUmbrella()
 {
-    customise_umbrella.style="animation='rotation 1.5s 1 ease-in-out'";
+    media_box.classList.add('rotate');
+    setTimeout(()=>{
+        media_box.classList.remove('rotate');
 
+    },1500)
+
+}
+
+function changeUmbrellaColor(imgSrc){
+    setTimeout(()=>{ 
+        customise_umbrella.src=imgSrc;
+    }, 700);
 }
 
 
 Array.from(colorOptions).forEach(colorOption=>{
-    colorOption.addEventListener('click',()=>{
+    colorOption.addEventListener('click',(e)=>{
+        if(e.target.id=='color-Pink')
+        {
+            changeUmbrellaColor('pink_umbrella.png');
+            colorSelects.style.borderColor='Pink'
+            
+        }
+        else if(e.target.id=='color-Blue')
+            {
+            changeUmbrellaColor('blue_umbrella.png');
+            colorSelects.style.borderColor='Blue'
+        }
+        else
+        {
+            changeUmbrellaColor('yellow_umbrella.png');
+            colorSelects.style.borderColor='Yellow'
+        }
+
+        rotateUmbrella();
 
     })
+})
+
+
+getLogo.addEventListener('change',(event)=>{
+    const file = event.target;
+    const reader = new FileReader();
+    
+    if(file)
+    {
+    
+    if(file.size > maxImgSize)
+    {
+        alert("File size exceeds a valid image file.");
+        getLogo=""; //clear the input;
+    }
+    else{
+        fileName= file.files[0].name;
+        reader.onload= function(e)
+    {
+        
+        display_logo.style.backgroungImage=`url(${e.target.result})`;
+    }
+    }
+    reader.readAsDataURL(file);
+
+    }
 })
